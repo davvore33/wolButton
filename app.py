518dc21c0a444b1a364c47189b06f5eb4407cda6
Matteo T <matteo.triggiani@egluetechnologies.com>
2022-08-01 10:42:17 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import configparser
+import os
+
+from flask import Flask, request, render_template
+
+configuration = configparser.ConfigParser()
+configuration.read(os.path.abspath("config.ini"))
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('wol') == 'WOL':
+            os.system('etherwake {}'.format(configuration['CONSTANTS']['hwaddr']))
+        else:
+            pass  # unknown
+    elif request.method == 'GET':
+        return render_template('index.html')
+
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
